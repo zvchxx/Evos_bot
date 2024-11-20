101155fe2b1b9535836e1084acff3b46ddd75892
zzvchx <abubakrrahmatullayev1001@gmail.com>
2024-11-20 18:08:04 +0500

    write import handlers

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from aiogram import executor
 from loader import dp
 from main.database import database
 from utils.notify_devs import send_notification_to_devs
+import handlers
+
+async def on_startup(dispatcher):
+    await database.connect()
+    await send_notification_to_devs(dispatcher)
 
 
 async def on_shutdown(dispatcher):
-    await send_notification_to_devs(dispatcher)
-
     await database.disconnect()
 
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_shutdown=on_shutdown, skip_updates=True)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+
